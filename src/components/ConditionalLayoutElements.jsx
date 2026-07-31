6c23352a75c9e2d1f53dae813433446e0073c2ed
Ahmed Raza <123raza83@gmail.com>
2026-07-31 19:31:56 +0500

    feat: create ConditionalLayoutElements component for flexible layout rendering

--- src/components/ConditionalLayoutElements.jsx
+++ src/components/ConditionalLayoutElements.jsx
@@ -1,8 +1,5 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
-
 export default function ConditionalLayoutElements({ children }) {
-  const pathname = usePathname();
   return <>{children}</>;
 }
