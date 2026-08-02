af8f8c0992d01bedf94fe77250a0b822a1a00fa7
Ahmed Raza <123raza83@gmail.com>
2026-08-02 16:39:40 +0500

    feat: add TrackingScripts component to initialize Facebook and TikTok tracking pixels

--- src/components/TrackingScripts.jsx
+++ src/components/TrackingScripts.jsx
@@ -12,13 +12,13 @@ export default function TrackingScripts({
     <>
       {facebookPixelId ? (
         <>
-          <script id="facebook-pixel-init" dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          <Script id="facebook-pixel-init" strategy="afterInteractive">
+            {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
 n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,
-'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '${facebookPixelId}');`
-          }} />
+'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '${facebookPixelId}');`}
+          </Script>
           <noscript>
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
@@ -33,9 +33,9 @@ t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,do
       ) : null}
 
       {tiktokPixelId ? (
-        <script id="tiktok-pixel" dangerouslySetInnerHTML={{
-          __html: `!function(w,d,t){w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"];ttq.setAndDefer=function(target,method){target[method]=function(){target.push([method].concat(Array.prototype.slice.call(arguments,0)));};};for(var i=0;i<ttq.methods.length;i++){ttq.setAndDefer(ttq,ttq.methods[i]);}ttq.load=function(pixelId,options){var url="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{};ttq._i[pixelId]=[];ttq._i[pixelId]._u=url;ttq._t=ttq._t||{};ttq._t[pixelId]=+new Date();ttq._o=ttq._o||{};ttq._o[pixelId]=options||{};options=document.createElement("script");options.type="text/javascript";options.async=true;options.src=url+"?sdkid="+pixelId+"&lib="+t;pixelId=document.getElementsByTagName("script")[0];pixelId.parentNode.insertBefore(options,pixelId);};ttq.load('${tiktokPixelId}');ttq.page();}(window,document,'ttq');`
-        }} />
+        <Script id="tiktok-pixel" strategy="afterInteractive">
+          {`!function(w,d,t){w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"];ttq.setAndDefer=function(target,method){target[method]=function(){target.push([method].concat(Array.prototype.slice.call(arguments,0)));};};for(var i=0;i<ttq.methods.length;i++){ttq.setAndDefer(ttq,ttq.methods[i]);}ttq.load=function(pixelId,options){var url="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{};ttq._i[pixelId]=[];ttq._i[pixelId]._u=url;ttq._t=ttq._t||{};ttq._t[pixelId]=+new Date();ttq._o=ttq._o||{};ttq._o[pixelId]=options||{};options=document.createElement("script");options.type="text/javascript";options.async=true;options.src=url+"?sdkid="+pixelId+"&lib="+t;pixelId=document.getElementsByTagName("script")[0];pixelId.parentNode.insertBefore(options,pixelId);};ttq.load('${tiktokPixelId}');ttq.page();}(window,document,'ttq');`}
+        </Script>
       ) : null}
 
       <TrackingPageView
